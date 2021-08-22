6013e6cde91014508a57b72de8ba5fb23afe4859
Dant777 <47300885+Dant777@users.noreply.github.com>
2021-08-22 11:04:05 +0300

    ДЗ 4

--- index.js
+++ index.js
@@ -1,71 +1,57 @@
-/*
-1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
+
+/*1. Написать функцию, преобразующую число в объект. 
+Передавая на вход число от 0 до 999, мы должны получить на выходе объект, 
+в котором в соответствующих свойствах описаны единицы, десятки и сотни. 
+Например, для числа 245 мы должны получить следующий объект: 
+{‘единицы’: 5, ‘десятки’: 4, ‘сотни’: 2}. 
+Если число превышает 999, необходимо выдать соответствующее сообщение с 
+помощью console.log и вернуть пустой объект.
+
 */
 
-let number = 2;
+printObjNumber();
 
-while (number < 100) {
-   if (isSimpleNumber(number)) {
-    console.log(number);
-   }
-    number++;
-}
+function printObjNumber() {
 
-function isSimpleNumber(number) {
-     
-    for (let i = 2; i < number; i++){
-        if (number % i == 0){
-            return false;
-        } 
-    }       
-    return true;
-}
-
-
-/*
-2. С этого урока начинаем работать с функционалом интернет-магазина. 
-Предположим, есть сущность корзины. Нужно реализовать функционал подсчета стоимости
- корзины в зависимости от находящихся в ней товаров. 
-3. Товары в корзине хранятся в массиве. Задачи:
-    a) Организовать такой массив для хранения товаров в корзине;
-    b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
-*/
-
-// let basketOfGoods = [
-// {name:"product1", price:100, amount:3},
-// {name:"product2", price:100, amount:1},
-// {name:"product3", price:100, amount:2}];
-
-// console.log(countBasketPrice(basketOfGoods));
-
-// function countBasketPrice(basketOfGoods) {
-//     let sum = 0;
-
-//     for (let i = 0; i < basketOfGoods.length; i++) {
+    for (let i = 0; i < 1001; i++) {
         
-//         sum += basketOfGoods[i].price * basketOfGoods[i].amount;
-//     }
+        if (i < 999) {
+            const numberArr = createNumberArr(i);
+            console.log(createObjNumber(numberArr));
+        }else{
+            console.log({})
+        }
+        
+    }
+}
 
-//     return sum;
-// }
+function createObjNumber(numberArr) {
+    const strArr = ["единицы", "десятки", "сотни"];
+    let objNumber = {};
+    for (let i = 0; i < numberArr.length; i++) {
+        
+        objNumber[strArr[i]] = numberArr[i];
+    }
+    return objNumber;
+}
 
-/*
- 4. *Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла
-*/
+function createNumberArr(number) {
+    let x = number;
+    let i = 10;
+    let numberArr = [];
+    if(x == 0){
+        numberArr.push(x);
+        return numberArr;
+    }
+    while (x >= i / 10)
+    {
+        const a = x % i;
+        const b = x % (i / 10);
+        const c = (i/10);
+        const number = (a - b) /c;
+        numberArr.push(number);
+        i *= 10;
+    }
 
-// for (let i = 0; i < 10; console.log(i++) ) {
-    
-// }
-
-/*
-5. *Нарисовать пирамиду с помощью console.log,
- как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5:
-*/
-
-// let count = "*";
-// let i =1;
-// while (i <= 20) {
-//     console.log(count);
-//     count += "*"
-//     i++;
-// }
+    return numberArr;
+}

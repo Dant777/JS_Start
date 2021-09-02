4b568518302f7b17e1a1f4a5d4a8b66014e1252f
Dant777 <47300885+Dant777@users.noreply.github.com>
2021-09-02 11:57:40 +0300

    ДЗ 7

--- Shopping cart/index.js
+++ Shopping cart/index.js
@@ -1,11 +1,8 @@
-/*
-2. Реализовать модуль корзины. Создать блок товаров и блок корзины. У каждого товара есть кнопка «Купить», 
-при нажатии на которую происходит добавление имени и цены товара в блок корзины. Корзина должна уметь считать общую сумму заказа.
-*/
-const totalCostCart = document.getElementById("totalCost");
-let countCost = 0;
-
-function addInCart (cost) {
-    countCost += cost;
-    totalCostCart.innerHTML = countCost;
-}
+import {
+  initializeGameField,
+  setUpKeyboardHandlers,
+  startGame,
+} from "./game.js";
+initializeGameField();
+setUpKeyboardHandlers();
+startGame();
